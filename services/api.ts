954cce56ce5e256e907c218ab3c360c2890a73c5
sahilchauhan123 <sahilchauhanc123@gmail.com>
2026-04-23 11:56:34 +0530

    api changed

--- services/api.ts
+++ services/api.ts
@@ -4,13 +4,14 @@ import axios from 'axios';
 // Set global default just in case
 axios.defaults.withCredentials = true;
 
-const storedBaseUrl = localStorage.getItem('apiBaseUrl');
-export const BASE_URL = storedBaseUrl || (import.meta.env.DEV ? 'http://165.232.47.156:4000/api/v1' : '/api');
+// const storedBaseUrl = localStorage.getItem('apiBaseUrl');
+export const BASE_URL = import.meta.env.DEV ? 'https://api.lasomaa.com/api/v1' : '/api';
 
 const api = axios.create({
     baseURL: BASE_URL,
     withCredentials: true,
 });
+
 
 // Ensure withCredentials is set for every request
 api.interceptors.request.use((config) => {
